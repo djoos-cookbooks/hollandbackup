45a6c9c3aeab3a3cf9cb7dab941b3bf16abb919c
David Joos <david.joos@gmail.com>
2013-12-24 11:41:38 +0000

    restore simple default

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,10 +5,4 @@
 # Copyright 2012-2013, David Joos
 #
 
-#TEMP
 include_recipe "hollandbackup::repository"
-
-include_recipe "hollandbackup::mysqldump"
-
-include_recipe "hollandbackup::main"
-include_recipe "hollandbackup::backupsets"
