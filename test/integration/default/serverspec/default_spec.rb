35026d79bb2e83f5b0cc98301558eab867f09d3c
Hart Hoover <hart.hoover@rackspace.com>
2014-07-11 13:18:58 -0500

    Add testing to cookbook

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,16 @@
+# Encoding: utf-8
+
+require_relative 'spec_helper'
+
+describe file('/usr/sbin/holland') do
+  it { should be_file }
+end
+
+describe file('/etc/holland/holland.conf') do
+  it { should be_file }
+  its(:content) { should match /backupsets = default/ }
+end
+
+describe package('holland') do
+  it { should be_installed }
+end
