6157ff79c36afd510eaa542537609064fb2726ee
Marco A. Morales <marcoamorales@gmail.com>
2015-05-13 13:43:41 -0500

    remove the permissions test
    centos and ubuntu have different default permissions for this file, so
    they won't match unless we modify the default configuration.

--- test/integration/default/serverspec/holland_install_spec.rb
+++ test/integration/default/serverspec/holland_install_spec.rb
@@ -17,7 +17,6 @@ describe file('/etc/holland/holland.conf') do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
-  it { should be_mode 644 }
   its(:content) { should match %r{backup_directory = \/var\/spool\/holland} }
 end
 
