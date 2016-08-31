5e4e57686896b65b5a991b136f32fa0b935b58b0
David Joos <david.joos@gmail.com>
2016-08-31 10:03:46 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'development@davidjoos.com'
 license 'MIT'
 description 'Installs/Configures hollandbackup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.2'
+version '0.3.0'
 
 %w(debian ubuntu centos redhat).each do |os|
   supports os
